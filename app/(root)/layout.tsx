152d2468f3e60b0dfa8a6078f9ca7e566b2b05ba
ViniciusMV <viniciuscordeiro789@gmail.com>
2025-02-11 14:59:42 -0300

    prostore project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/shared/header';
+import Footer from '@/components/footer';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'  suppressHydrationWarning>
+      <body >
+        <div className='flex h-screen flex-col'>
+          <Header />
+          <main className='flex-1 wrapper'>{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ components/product/product-list.tsx
@@ -0,0 +1,31 @@
+import ProductCard from './product-card';
+
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any[];
+  title?: string;
+  limit?: number;
+}) => {
+  // Apply limit if provided, otherwise show all products
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {limitedData.length > 0 ? (
+        limitedData.map((product: any) => (
+          <ProductCard key={product.slug} product={product} />
+        ))
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
